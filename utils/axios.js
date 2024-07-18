b1984e4ac5d1869ca9ed2d0d9d1f3707436d1f5d
RitikShah09 <ritiksbs@gmail.com>
2024-07-19 00:38:35 +0530

    updated

--- utils/axios.js
+++ utils/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // "https://x-backend-lqbp.onrender.com/"
   // baseURL:"https://internshala-8a7j.onrender.com/",
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://x-backend-lqbp.onrender.com/",
   withCredentials: true,
 });
 
